eac10987f31432ade3eac768380a240aada55ead
libraryconnect <libraryconnect@users.noreply.github.com>
2015-07-26 07:20:53 +0530

    Entered code
    R script code entered as per coursera assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions calculate the inverse of a matrix and cache the result
+## to descrease processing time. 
 
-## Write a short comment describing this function
+## makeCachematrix just creates a list that stores the functions and data.
+## This has to be run first. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  z <<- list(get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+  print("Done")
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if there is a cached result before calculating a new
+## result.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- z$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- z$get()
+  m <- solve(data)
+  z$setsolve(m)
+  m
 }
